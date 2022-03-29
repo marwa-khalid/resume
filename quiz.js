67233e265fae6ffa5846d1024acfdfdb4fa662e7
Marwa Khalid <77286253+marwa-khalid@users.noreply.github.com>
2022-03-29 12:20:12 +0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function changeBg(){
     var navbar = document.getElementById('navbar');
     var scrollValue = window.scrollY;
@@ -9,17 +8,3 @@ function changeBg(){
 }
 
 window.addEventListener('scroll',changeBg);
-
-=======
-function changeBg(){
-    var navbar = document.getElementById('navbar');
-    var scrollValue = window.scrollY;
-    if(scrollValue<150)
-        navbar.classList.remove('bgColor');
-    else
-    navbar.classList.add('bgColor');
-}
-
-window.addEventListener('scroll',changeBg);
-
->>>>>>> 68860e5ed5a841a589dbf944242d7f53a0bba7e7
